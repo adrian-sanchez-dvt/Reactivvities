d8fccc077eb3d772d1c50f4b12390243ad4d73b0
ADRIAN SANCHEZ RODRIGUEZ <adrian.sanchez@external.toyota.es>
2025-10-20 12:37:36 +0200

    End of section 5

---
+++ client/src/features/activities/dashboard/ActivityCard.tsx
@@ -0,0 +1,64 @@
+import {
+  Box,
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Chip,
+  Typography,
+} from "@mui/material";
+
+type Props = {
+  activity: Activity;
+  selectActivity: (id: string) => void;
+  deleteActivity: (id: string) => void;
+};
+
+export const ActivityCard = ({
+  activity,
+  selectActivity,
+  deleteActivity,
+}: Props) => {
+  return (
+    <Card sx={{ borderRadius: 3 }}>
+      <CardContent>
+        <Typography variant="h5">{activity.title}</Typography>
+        <Typography sx={{ color: "text.secondary", mb: 1 }}>
+          {activity.date.toString()}
+        </Typography>
+        <Typography variant="body2">{activity.description}</Typography>
+        <Typography variant="subtitle1">
+          {activity.city} / {activity.venue}
+        </Typography>
+        <CardActions
+          sx={{ display: "flex", justifyContent: "space-between", pb: 2 }}
+        >
+          <Chip
+            label={activity.category}
+            variant="outlined"
+          />
+          <Box
+            display="flex"
+            gap={3}
+          >
+            <Button
+              size="medium"
+              variant="contained"
+              onClick={() => selectActivity(activity.id)}
+            >
+              View
+            </Button>
+            <Button
+              size="medium"
+              variant="contained"
+              color="error"
+              onClick={() => deleteActivity(activity.id)}
+            >
+              Delete
+            </Button>
+          </Box>
+        </CardActions>
+      </CardContent>
+    </Card>
+  );
+};
