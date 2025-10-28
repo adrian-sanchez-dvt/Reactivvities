0fd8c3a6797b0a5a04577bec92668f7ff52893a2
ADRIAN SANCHEZ RODRIGUEZ <adrian.sanchez@external.toyota.es>
2025-10-28 14:41:01 +0100

    End of section 12

--- API/Controllers/ActivitiesController.cs
+++ API/Controllers/ActivitiesController.cs
@@ -4,6 +4,7 @@ using Application.Activities.DTOs;
 using Application.Activities.Queries;
 using Domain;
 using MediatR;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace API.Controllers;
@@ -17,6 +18,7 @@ public class ActivitiesController() : BaseApiController
     //     this.context = context;
     // }
 
+    [AllowAnonymous]
     [HttpGet]
     public async Task<ActionResult<List<Activity>>> GetActivities()
     {
